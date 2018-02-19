4d26a1047acb3b67cfc5fa732bb66d482dfffa20
zakrywilson <zakrywilson@gmail.com>
2018-02-18 21:39:56 -0700

    Created DVD library service

---
+++ server/src/main/java/com/wilson/dvd/library/DvdLibraryApplication.java
@@ -0,0 +1,18 @@
+package com.wilson.dvd.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Launches application.
+ *
+ * @author Zach Wilson
+ */
+@SpringBootApplication
+public class DvdLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvdLibraryApplication.class, args);
+    }
+
+}
