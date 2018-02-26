51ed748b39f205d985719c4c54c2ff92117c6780
zakrywilson <zakrywilson@gmail.com>
2018-02-25 17:43:31 -0700

    Create movie service interface

---
+++ server/src/main/java/com/wilson/movie/library/service/MovieService.java
@@ -0,0 +1,34 @@
+package com.wilson.movie.library.service;
+
+import com.wilson.movie.library.domain.MovieEntity;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+/**
+ * Outlines the implementation of a movie service.
+ *
+ * @author Zach Wilson
+ */
+public interface MovieService {
+
+    MovieEntity create(MovieEntity movie);
+
+    MovieEntity getById(Integer id);
+    MovieEntity getByTitle(String title);
+    Collection<MovieEntity> getAllByReleaseDate(LocalDate releaseDate);
+    Collection<MovieEntity> getAllByReleaseDate(Integer releaseDateEpochDay);
+    Collection<MovieEntity> getAllByStudio(String studio);
+    Collection<MovieEntity> getAllById(Collection<Integer> ids);
+    Collection<MovieEntity> getAll();
+
+    MovieEntity update(Integer id, MovieEntity movie);
+
+    Integer deleteById(Integer id);
+    Collection<Integer> deleteAllById(Collection<Integer> ids);
+    Collection<Integer> deleteAll();
+
+    boolean exists(Integer id);
+    boolean exists(String title);
+
+}
