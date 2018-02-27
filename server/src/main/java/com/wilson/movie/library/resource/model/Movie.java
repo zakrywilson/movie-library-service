a8262ed17c892da25f26892b75dc99ceda304c95
zakrywilson <zakrywilson@gmail.com>
2018-02-26 19:58:56 -0700

    Added more JSON properties for better representation the intended structure

--- server/src/main/java/com/wilson/movie/library/resource/model/Movie.java
+++ server/src/main/java/com/wilson/movie/library/resource/model/Movie.java
@@ -1,7 +1,12 @@
 package com.wilson.movie.library.resource.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.wilson.movie.library.resource.utils.Deserializers;
+import com.wilson.movie.library.resource.utils.Serializers;
 import lombok.Builder;
 import lombok.Value;
 import lombok.experimental.Wither;
@@ -27,6 +32,9 @@ public final class Movie {
     private final String title;
 
     @JsonProperty("release_date")
+    @JsonSerialize(using = Serializers.LocalDateSerializer.class)
+    @JsonDeserialize(using = Deserializers.LocalDateDeserializer.class)
+    @JsonPropertyDescription("ISO date format, e.g., 1980-5-23")
     @Wither
     private final LocalDate releaseDate;
 
